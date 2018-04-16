cf8552ccadbe932824d475f3d71531ff7aa5a724
ryanbas21 <ryanbas21@gmail.com>
2018-04-16 00:31:19 -0500

    bundle-analyzer

---
+++ next.config.js
@@ -0,0 +1,22 @@
+const {
+	WebpackBundleSizeAnalyzerPlugin
+} = require('webpack-bundle-size-analyzer');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const { ANALYZE } = process.env;
+
+module.exports = {
+	webpack: function(config, { isServer }) {
+		if (ANALYZE) {
+			config.plugins.push(new WebpackBundleSizeAnalyzerPlugin('stats.txt'));
+			config.plugins.push(
+				new BundleAnalyzerPlugin({
+					analyzerMode: 'server',
+					analyzerPort: isServer ? 8888 : 8889,
+					openAnalyzer: true
+				})
+			);
+		}
+
+		return config;
+	}
+};
